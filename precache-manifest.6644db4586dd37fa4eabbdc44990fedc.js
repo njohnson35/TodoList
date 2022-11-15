6271fdb22a3d191c2b7d3d6f07e73a81c9021df8
njohnson35 <112568197+njohnson35@users.noreply.github.com>
2022-11-15 08:55:34 -0600

    Updates

---
+++ precache-manifest.6644db4586dd37fa4eabbdc44990fedc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5b18c43a8bf56fb83eb",
+    "url": "/TodoList/static/css/main.35c7674e.chunk.css"
+  },
+  {
+    "revision": "c5b18c43a8bf56fb83eb",
+    "url": "/TodoList/static/js/main.c5b18c43.chunk.js"
+  },
+  {
+    "revision": "5b845fa3b8e60aebc799",
+    "url": "/TodoList/static/js/1.5b845fa3.chunk.js"
+  },
+  {
+    "revision": "de07b96509ca169f82f9",
+    "url": "/TodoList/static/js/2.de07b965.chunk.js"
+  },
+  {
+    "revision": "fa66112fa7193a76ba1e",
+    "url": "/TodoList/static/js/runtime~main.fa66112f.js"
+  },
+  {
+    "revision": "d24adbe757e00431ce47d082f52615ba",
+    "url": "/TodoList/index.html"
+  }
+];
